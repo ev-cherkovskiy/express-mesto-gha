1109e48b5b0ff2525f0a9d0990eb29d4faf78218
Evgeny Cherkovskiy <ev.cherkovskiy@yandex.ru>
2022-06-17 21:44:19 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -78,7 +78,7 @@ const login = (req, res) => {
   return User.findUserByCredentials(email, password)
     .then(user => {
       // create jwt
-      const token = jwt.sign({ _id: user._id }, '', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'secret-key', { expiresIn: '7d' });
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,15 +6,15 @@ module.exports = (req, res, next) => {
   if (!token) {
     return res
       .status(401)
-      .send({ message: 'Необходима авторизация ' });
+      .send({ message: 'Необходима авторизация' });
   };
   let payload;
   try {
-    payload = jwt.verify(token, '');
+    payload = jwt.verify(token, 'secret-key');
   } catch (err) {
     return res
       .status(401)
-      .send({ message: 'Необходима авторизация ' });
+      .send({ message: 'Необходима авторизация' });
   };
   req.user = payload;
   next();
